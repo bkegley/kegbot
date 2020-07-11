5b33cb74c8c17cdb51fbe2cd1686a104493e68d8
Bryan Kegley <hi@bryankegley.me>
2020-07-11 09:14:41 -0500

    remove svelte files

--- packages/server/src/service/ICommandService.ts
+++ packages/server/src/service/ICommandService.ts
@@ -4,5 +4,5 @@ export interface ICommandService {
   listCommands(): Promise<Command[] | undefined>;
   getCommand(command: string): Promise<Command | undefined>;
   create(input: { command: string; response: string }): Promise<Command>;
-  delete(id: string): Promise<string>;
+  delete(id: string): Promise<{ id: string }>;
 }
