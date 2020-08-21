16cef72ac097ab753756815b0d6a66ac07f23ee5
Bryan Kegley <hi@bryankegley.me>
2020-08-21 17:13:16 -0500

    Fixing Docker for deployment

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -2,16 +2,28 @@ import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConne
 
 const port: number = ((process.env.POSTGRES_PORT as unknown) as number) ?? 5432;
 
-export const ormConfig: PostgresConnectionOptions = {
-  type: "postgres",
-  host: process.env.POSTGRES_HOST ?? "localhost",
-  port,
-  username: process.env.POSTGRES_USER ?? "postgres",
-  password: process.env.POSTGRES_PASSWORD ?? "postgres",
-  database: process.env.POSTGRES_DB ?? "postgres",
-  synchronize: true,
-  logging: false,
-  entities: ["src/entity/**/*.ts"],
-  migrations: ["src/migration/**/*.ts"],
-  subscribers: ["src/subscriber/**/*.ts"]
-};
+const DATABASE_URL = process.env.DATABASE_URL;
+
+export const ormConfig: PostgresConnectionOptions = DATABASE_URL
+  ? {
+      type: "postgres",
+      url: DATABASE_URL,
+      synchronize: true,
+      logging: false,
+      entities: ["src/entity/**/*.ts"],
+      migrations: ["src/migration/**/*.ts"],
+      subscribers: ["src/subscriber/**/*.ts"]
+    }
+  : {
+      type: "postgres",
+      host: process.env.POSTGRES_HOST ?? "localhost",
+      port,
+      username: process.env.POSTGRES_USER ?? "postgres",
+      password: process.env.POSTGRES_PASSWORD ?? "postgres",
+      database: process.env.POSTGRES_DB ?? "postgres",
+      synchronize: true,
+      logging: false,
+      entities: ["src/entity/**/*.ts"],
+      migrations: ["src/migration/**/*.ts"],
+      subscribers: ["src/subscriber/**/*.ts"]
+    };
